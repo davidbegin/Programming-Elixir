cf8bb16af73ef1ced39d8c34ec6e761649db2bae
David Begin <davidmichaelbe@gmail.com>
2015-12-23 23:03:22 -0800

    extract displaying of unfold into function

--- unfold.exs
+++ unfold.exs
@@ -12,6 +12,15 @@ defmodule Unfolder do
     Stream.unfold({ 0, 1 }, unfold_fn)
     |> Enum.take(10)
   end
+
+  def display(f1, f2, func) do
+    IO.puts "\ec"
+    IO.puts "\e[33m#{func}\e[0m"
+    IO.puts "\e[33m\nf1\e[0m\nf2\n\e[0m"
+    IO.puts String.duplicate("\e[33m0\e[0m", f1)
+    IO.puts String.duplicate("0", f2)
+    :timer.sleep(300)
+  end
 end
 
 # Simple version
@@ -19,12 +28,7 @@ unfold_fn = fn { f1, f2 } -> { f1, { f2, f2 + 1 } } end
 
 # Verbose version
 unfold_fn = fn { f1, f2 } ->
-  IO.puts "\ec"
-  IO.puts "\e[33m{ f1, { f2, f2 + 1 } }\e[0m"
-  IO.puts "\e[33m\nf1\e[0m\nf2\n\e[0m"
-  IO.puts String.duplicate("\e[33m 0 \e[0m", f1)
-  IO.puts String.duplicate(" 0 ", f2)
-  :timer.sleep(300)
+  Unfolder.display(f1, f2, "{ f1, { f2, f2 + 1 } }")
   { f1, { f2, f2 + 1 } }
 end
 
@@ -35,12 +39,7 @@ unfold_fn = fn { f1, f2 } -> { f1, { f2, f1 + f2 } } end
 
 # Verbose version
 unfold_fn = fn { f1, f2 } ->
-  IO.puts "\ec"
-  IO.puts "\e[33m{ f1, { f2, f1 + f2 } }\e[0m"
-  IO.puts "\e[33m\nf1\e[0m\nf2\n\e[0m"
-  IO.puts String.duplicate("\e[33m0\e[0m", f1)
-  IO.puts String.duplicate("0", f2)
-  :timer.sleep(300)
+  Unfolder.display(f1, f2, "{ f1, { f2, f1 + f2 } }")
   { f1, { f2, f1 + f2 } }
 end
 
@@ -51,12 +50,6 @@ unfold_fn = fn { f1, f2 } -> { f1 * 2, { f2, f1 + f2 } } end
 
 # Verbose version
 unfold_fn = fn { f1, f2 } ->
-  IO.puts "\ec"
-  IO.puts "\e[33m{ f1, { f2, f1 + f2 } }\e[0m"
-  IO.puts "\e[33m\nf1\e[0m\nf2\n\e[0m"
-  IO.puts String.duplicate("\e[33m0\e[0m", f1)
-  IO.puts String.duplicate("0", f2)
-  :timer.sleep(300)
   { f1, { f2 * 2, f1 + f2 } }
 end
 
