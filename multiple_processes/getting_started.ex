59b2e3d8e9630e801a49dc7743eda10af0a404bb
David Begin <davidmichaelbe@gmail.com>
2016-01-07 22:12:05 -0800

    more practice around simple sending messages

--- multiple_processes/getting_started.ex
+++ multiple_processes/getting_started.ex
@@ -36,6 +36,68 @@ receive do
     IO.puts msg
 end
 
-# -----------------------------------------------
+IO.puts "\n=====================================\n"
 
+defmodule Greeter2 do
+  def greet do
+    receive do
+      { sender, msg } ->
+        send sender, { :ok, "Hello there, #{msg}" }
+    end
+  end
+end
 
+pid = spawn(Greeter2, :greet, [])
+
+send pid, { self, "I'm sending messages" }
+
+receive do
+  { :ok, msg } ->
+    IO.puts msg
+end
+
+send pid, { self, "I'm sending a second message" }
+
+# This will timeout, because Greeter2 only knows how to handle a single
+# message, and then it exits, meaning another message will never
+# be received.
+
+# so after, takes care of just moving on after waiting a
+# specified amount of time. In this case 500 ms
+receive do
+  { :ok, msg } ->
+    IO.puts msg
+after 500 ->
+  IO.puts "Timed out waiting for a message!"
+end
+
+IO.puts "\n=====================================\n"
+
+# So now we are going to make module
+# that will continue to take messages and not
+# exit after just 1.
+defmodule Greeter3 do
+  def greet do
+    receive do
+      { sender, msg } ->
+        send sender, { :ok, "Your message: #{msg} "}
+      greet
+    end
+  end
+end
+
+pid = spawn(Greeter3, :greet, [])
+
+send pid, { self, "Greeter3 in the house!" }
+
+receive do
+  { :ok, msg } ->
+    IO.puts msg
+end
+
+send pid, { self, "Another One" }
+
+receive do
+  { :ok, msg } ->
+    IO.puts msg
+end
